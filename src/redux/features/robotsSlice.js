2ebb00aa950110d0a5a39ed285b3328967202208
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-05-15 15:19:19 +0200

    Created deleteRobotActionCreator on robotsSlice

--- src/redux/features/robotsSlice.js
+++ src/redux/features/robotsSlice.js
@@ -5,9 +5,14 @@ const robotsSlice = createSlice({
   initialState: [],
   reducers: {
     loadRobots: (robots, action) => [...action.payload],
+    deleteRobot: (robots, action) =>
+      robots.filter((robot) => robot._id !== action.payload),
   },
 });
 
 export default robotsSlice.reducer;
 
-export const { loadRobots: loadRobotsActionCreator } = robotsSlice.actions;
+export const {
+  loadRobots: loadRobotsActionCreator,
+  deleteRobot: deleteRobotActionCreator,
+} = robotsSlice.actions;
